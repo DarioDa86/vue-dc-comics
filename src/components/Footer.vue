186b4cc8b94cb04c0fdb9dfeb2be8b402e280804
DarioDa86 <dario.dagnelli@gmail.com>
2021-09-30 22:17:57 +0200

    Fixato footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,62 @@
   <footer>
     <div class="container">
       <div class="topFooter">
+        <div class="footerLeft">
+          <div class="col-1">
+            <h3>DC COMICS</h3>
+            <ul>
+              <li><a href="#">Charracters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+            <h3>SHOP</h3>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <div class="col-2">
+            <h3>DC</h3>
+            <ul>
+              <li><a href="#">Therms Of Use</a></li>
+              <li><a href="#">Privacy policy(New</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscription</a></li>
+              <li><a href="#">Talent Workshops</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">COntact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-3">
+            <h3>SITES</h3>
+            <ul>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="footerRight">
+          
+        </div>
 
       </div>
 
-      <div class="bottomFooter">
+
+    </div>
+    <div class="bgFooter">
+      <div class="bottomFooter container">
 
         <div class="signUp-Btn">
           <button>SIGN-UP NOW!</button>
@@ -13,18 +65,15 @@
 
         <div class="socialIcons">
           <div class="textIcons">FOLLOW US</div>
-          <img src="@/assets/images/footer-facebook.png" alt="Facebook">
-          <img src="@/assets/images/footer-twitter.png" alt="Twitter">
-          <img src="@/assets/images/footer-youtube.png" alt="Youtube">
-          <img src="@/assets/images/footer-pinterest.png" alt="Pinterest">
-          <img src="@/assets/images/footer-periscope.png" alt="Periscope">
-
+          <a href="#"><img src="../assets/images/footer-facebook.png" alt="Facebook"></a>
+          <a href="#"><img src="../assets/images/footer-twitter.png" alt="Twitter"></a>
+          <a href="#"><img src="../assets/images/footer-youtube.png" alt="Youtube"></a>
+          <a href="#"><img src="../assets/images/footer-pinterest.png" alt="Pinterest"></a>
+          <a href="#"><img src="../assets/images/footer-periscope.png" alt="Periscope"></a>
         </div>
-
-
       </div>
-
     </div>
+
   </footer>
 </template>
 
@@ -38,19 +87,79 @@ export default {
 @import '../assets/style/variables';
 
 footer {
-  background-color: $footerBottomBackColor;
+  background-image: url(../assets/images/footer-bg.jpg);
 
-  .bottomFooter {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    vertical-align: center;
-    padding: 20px 0;
-
-    .socialIcons {
+  .topFooter {
       display: flex;
+
+    .footerLeft {
+      display: flex;
+      color: #fff;
+      width: 30%;
+      margin: 30px 0;
+
+      .col-1{
+        width: calc(100% / 3);
+      }
+      
+      .col-2{
+        width: calc(100% / 3);
+      }
+
+      .col-3{
+        width: calc(100% / 3);
+      }
+    }
+
+    .footerRight {
+      width: 70%;
+      background-image: url(../assets/images/dc-logo-bg.png);
+      background-repeat: no-repeat;
+      background-position: right center; 
+
     }
   }
-}
 
+    .bgFooter {
+      background-color: $footerBottomBackColor;
+
+
+      .bottomFooter {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px 0;
+        // background-color: $footerBottomBackColor;
+
+        .signUp-Btn{
+
+          button {
+            padding: 15px 10px;
+            border-radius: 0;
+            border: 3px solid $cyanBandColor;
+            background-color: transparent;
+            color: #fff;
+            font-size: 16px;
+          }
+        }
+
+        .socialIcons {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          color: $cyanBandColor;
+
+          img {
+            margin: 0 10px;
+          }
+
+          .textIcons {
+            font-size: 18px;
+            font-weight: bold;
+            margin-right: 15px;
+          }
+        }
+      }
+    }
+}
 </style>
